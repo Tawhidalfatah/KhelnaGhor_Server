93100b063fe5e0ad42005a2861f5e53eb1ad0b1f
D9dman <tawhid268@gmail.com>
2023-05-20 10:57:44 +0600

    single toy route server side added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -39,6 +39,14 @@ async function run() {
       }
       const result = await toyCollection.find(query).limit(20).toArray();
 
+      res.send(result);
+    });
+
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
       res.send(result);
     });
 
